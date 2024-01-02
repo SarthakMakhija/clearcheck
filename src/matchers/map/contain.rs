e4ace155ec30225c082eea763dc56b16f1008dc1
Sarthak <sarthak.makhija@gmail.com>
2024-01-02 19:52:13 +0530

    Sarthak | Adds value based matchers for HashMap

--- src/matchers/map/contain.rs
+++ src/matchers/map/contain.rs
@@ -15,6 +15,16 @@ trait KeyContains<K, V> {
               Q: Hash + Eq + Debug;
 }
 
+trait ValueContains<K, V> {
+    fn should_contain_value<S>(&self, value: &S) -> &Self
+        where V: Borrow<S> + PartialEq<S>,
+              S: Eq + Debug;
+
+    fn should_not_contain_value<S>(&self, value: &S) -> &Self
+        where V: Borrow<S> + PartialEq<S>,
+              S: PartialOrd + Debug;
+}
+
 trait KeyValueContains<K, V> {
     fn should_contain<Q, S>(&self, key: &Q, value: &S) -> &Self
         where K: Borrow<Q>,
@@ -47,6 +57,32 @@ impl<K, V> KeyContains<K, V> for HashMap<K, V>
         let contains = self.contains_key(key);
         if contains {
             assert_failed_binary(AssertKind::NotContains, &self.keys(), key);
+        }
+        self
+    }
+}
+
+impl<K, V> ValueContains<K, V> for HashMap<K, V>
+    where K: Hash + Eq + PartialEq + Debug,
+          V: Eq + Debug {
+    fn should_contain_value<S>(&self, value: &S) -> &Self
+        where
+            V: Borrow<S> + PartialEq<S>,
+            S: Eq + Debug {
+        let contains = self.values().any(|source| source == value);
+        if !contains {
+            assert_failed_binary(AssertKind::ContainsValue, &self.values(), value);
+        }
+        self
+    }
+
+    fn should_not_contain_value<S>(&self, value: &S) -> &Self
+        where
+            V: Borrow<S> + PartialEq<S>,
+            S: PartialOrd + Debug {
+        let contains = self.values().any(|source| source == value);
+        if contains {
+            assert_failed_binary(AssertKind::NotContainsValue, &self.values(), value);
         }
         self
     }
@@ -121,6 +157,43 @@ mod key_contains_tests {
         let mut key_value = HashMap::new();
         key_value.insert("rust", "assert");
         key_value.should_not_contain_key(&"rust");
+    }
+}
+
+#[cfg(test)]
+mod value_contains_tests {
+    use std::collections::HashMap;
+
+    use crate::matchers::map::contain::ValueContains;
+
+    #[test]
+    fn should_contain_value() {
+        let mut key_value = HashMap::new();
+        key_value.insert("rust", "assert");
+        key_value.should_contain_value(&"assert");
+    }
+
+    #[test]
+    #[should_panic]
+    fn should_contain_value_but_it_did_not() {
+        let mut key_value = HashMap::new();
+        key_value.insert("rust", "assert");
+        key_value.should_contain_value(&"java");
+    }
+
+    #[test]
+    fn should_not_contain_value() {
+        let mut key_value = HashMap::new();
+        key_value.insert("rust", "assert");
+        key_value.should_not_contain_value(&"catch");
+    }
+
+    #[test]
+    #[should_panic]
+    fn should_not_contain_value_but_it_contained() {
+        let mut key_value = HashMap::new();
+        key_value.insert("rust", "assert");
+        key_value.should_not_contain_value(&"assert");
     }
 }
 
