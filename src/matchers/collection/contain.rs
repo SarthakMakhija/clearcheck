59176a7b7b956fcbaec2ddb74ec639b40f11bafd
Sarthak <sarthak.makhija@gmail.com>
2024-01-03 13:12:47 +0530

    Sarthak | Changes constraints in Contains in collection

--- src/matchers/collection/contain.rs
+++ src/matchers/collection/contain.rs
@@ -1,28 +1,42 @@
+use std::borrow::Borrow;
+use std::fmt::Debug;
+
 use crate::panicking::{assert_failed_binary, AssertKind};
 
 pub trait Contains<T>
-where
-    T: Eq + std::fmt::Debug,
+    where
+        T: Eq + Debug,
 {
-    fn should_contain(&self, element: &T) -> &Self;
-    fn should_not_contain(&self, element: &T) -> &Self;
+    fn should_contain<Q>(&self, element: &Q) -> &Self
+        where
+            T: Borrow<Q>,
+            Q: Eq + Debug + ?Sized;
+    fn should_not_contain<Q>(&self, element: &Q) -> &Self
+        where T: Borrow<Q>,
+              Q: Eq + Debug + ?Sized;
 }
 
 impl<T> Contains<T> for Vec<T>
-where
-    T: std::fmt::Debug,
-    T: Eq,
+    where
+        T: Debug,
+        T: Eq,
 {
-    fn should_contain(&self, element: &T) -> &Self {
-        let contains = self.iter().any(|source| source == element);
+    fn should_contain<Q>(&self, element: &Q) -> &Self
+        where T: Borrow<Q>,
+              Q: Eq + Debug + ?Sized
+    {
+        let contains = self.iter().any(|source| source.borrow() == element);
         if !contains {
             assert_failed_binary(AssertKind::Contains, self, element);
         }
         self
     }
 
-    fn should_not_contain(&self, element: &T) -> &Self {
-        let contains = self.iter().any(|source| source == element);
+    fn should_not_contain<Q>(&self, element: &Q) -> &Self
+        where T: Borrow<Q>,
+              Q: Eq + Debug  + ?Sized
+    {
+        let contains = self.iter().any(|source| source.borrow() == element);
         if contains {
             assert_failed_binary(AssertKind::NotContains, &self, element);
         }
@@ -37,26 +51,26 @@ mod tests {
     #[test]
     fn should_contain() {
         let collection = vec!["junit", "assert4j", "catch2"];
-        collection.should_contain(&"assert4j");
+        collection.should_contain("assert4j");
     }
 
     #[test]
     #[should_panic]
     fn should_contain_but_was_not_contained() {
         let collection = vec!["junit", "assert4j", "catch2"];
-        collection.should_contain(&"catch");
+        collection.should_contain("catch");
     }
 
     #[test]
     fn should_not_contain() {
         let collection = vec!["junit", "assert4j", "catch2"];
-        collection.should_not_contain(&"catch");
+        collection.should_not_contain("catch");
     }
 
     #[test]
     #[should_panic]
     fn should_not_contain_but_was_contained() {
         let collection = vec!["junit", "assert4j", "catch2"];
-        collection.should_not_contain(&"catch2");
+        collection.should_not_contain("catch2");
     }
 }
