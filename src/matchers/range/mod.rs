e68ba52732295bcba47c8fb1f01afe368a8c4c25
Sarthak <sarthak.makhija@gmail.com>
2024-01-04 19:01:01 +0530

    Sarthak | Adds tests for range matchers

--- src/matchers/range/mod.rs
+++ src/matchers/range/mod.rs
@@ -26,3 +26,36 @@ pub fn be_in_inclusive_range<T: PartialOrd>(range: &RangeInclusive<T>) -> RangeB
 pub fn be_in_exclusive_range<T: PartialOrd>(range: &Range<T>) -> RangeBased<T> {
     RangeBased::HalfOpen(range)
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::assertions::bool::TrueFalse;
+    use crate::matchers::range::{be_in_exclusive_range, be_in_inclusive_range};
+    use crate::matchers::Matcher;
+
+    #[test]
+    fn should_be_in_inclusive_range() {
+        let matcher = be_in_inclusive_range(&(1..=4));
+        matcher.test(&2).should_be_true();
+    }
+
+    #[test]
+    #[should_panic]
+    fn should_be_in_inclusive_range_but_was_not() {
+        let matcher = be_in_inclusive_range(&(1..=4));
+        matcher.test(&5).should_be_true();
+    }
+
+    #[test]
+    fn should_be_in_exclusive_range() {
+        let matcher = be_in_exclusive_range(&(1..4));
+        matcher.test(&2).should_be_true();
+    }
+
+    #[test]
+    #[should_panic]
+    fn should_be_in_exclusive_range_but_was_not() {
+        let matcher = be_in_exclusive_range(&(1..4));
+        matcher.test(&4).should_be_true();
+    }
+}
