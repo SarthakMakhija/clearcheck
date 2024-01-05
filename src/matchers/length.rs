be177c84670b21674c0e7dc17fe503b18c366187
Sarthak <sarthak.makhija@gmail.com>
2024-01-05 13:33:04 +0530

    Sarthak | Refactors LengthBased matcher

--- src/matchers/length.rs
+++ src/matchers/length.rs
@@ -11,18 +11,18 @@ pub enum LengthBased {
 
 impl LengthBased {
     pub fn test_slice<T>(&self, collection: &[T]) -> MatcherResult {
-        self.test_inner(collection.len(), "Collection")
+        self.test(collection.len(), "Collection")
     }
 
     pub fn test_map<K: Hash + Eq, V>(&self, collection: &HashMap<K, V>) -> MatcherResult {
-        self.test_inner(collection.len(), "Collection")
+        self.test(collection.len(), "Collection")
     }
 
     pub fn test_string(&self, collection: &str) -> MatcherResult {
-        self.test_inner(collection.len(), "String")
+        self.test(collection.len(), "String")
     }
 
-    fn test_inner(&self, input_length: usize, message_prefix: &'static str) -> MatcherResult {
+    fn test(&self, input_length: usize, message_prefix: &'static str) -> MatcherResult {
         match self {
             LengthBased::Same(length) => MatcherResult::formatted(
                 input_length == *length,
@@ -83,39 +83,39 @@ mod tests {
     #[test]
     fn should_have_same_length() {
         let matcher = have_same_length(4);
-        matcher.test_inner(4, "Collection").passed.should_be_true();
+        matcher.test(4, "Collection").passed.should_be_true();
     }
 
     #[test]
     #[should_panic]
     fn should_have_same_length_but_was_not() {
         let matcher = have_same_length(4);
-        matcher.test_inner(2, "Collection").passed.should_be_true();
+        matcher.test(2, "Collection").passed.should_be_true();
     }
 
     #[test]
     fn should_have_atleast_same_length() {
         let matcher = have_atleast_same_length(4);
-        matcher.test_inner(5, "Collection").passed.should_be_true();
+        matcher.test(5, "Collection").passed.should_be_true();
     }
 
     #[test]
     #[should_panic]
     fn should_have_atleast_same_length_but_was_not() {
         let matcher = have_atleast_same_length(4);
-        matcher.test_inner(2, "Collection").passed.should_be_true();
+        matcher.test(2, "Collection").passed.should_be_true();
     }
 
     #[test]
     fn should_have_atmost_length() {
         let matcher = have_atmost_same_length(4);
-        matcher.test_inner(3, "Collection").passed.should_be_true();
+        matcher.test(3, "Collection").passed.should_be_true();
     }
 
     #[test]
     #[should_panic]
     fn should_have_atmost_length_but_was_not() {
         let matcher = have_atmost_same_length(4);
-        matcher.test_inner(5, "Collection").passed.should_be_true();
+        matcher.test(5, "Collection").passed.should_be_true();
     }
 }
