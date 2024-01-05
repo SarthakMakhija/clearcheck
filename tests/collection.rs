f50d0b738ffc73cf64dc50815b51b4fabdd93909
Sarthak <sarthak.makhija@gmail.com>
2024-01-05 19:59:35 +0530

    Sarthak | Adds an integration test for collection

---
+++ tests/collection.rs
@@ -0,0 +1,51 @@
+use assert4rs::assertions::collection::membership::Membership;
+use assert4rs::assertions::collection::size::Size;
+
+#[derive(Eq, Debug, PartialEq)]
+struct Book {
+    id: usize,
+    title: &'static str,
+}
+
+impl Book {
+    fn new(id: usize, title: &'static str) -> Self {
+        Book { id, title }
+    }
+}
+
+#[test]
+fn should_match_all() {
+    let library = vec![
+        Book::new(1, "Database internals"),
+        Book::new(2, "Designing data intensive applications"),
+        Book::new(3, "Learning rust"),
+        Book::new(4, "Rust in action"),
+    ];
+
+    library
+        .should_not_be_empty()
+        .should_have_at_least_size(3)
+        .should_contain_all(&[
+            &Book::new(3, "Learning rust"),
+            &Book::new(4, "Rust in action"),
+        ]);
+}
+
+#[test]
+#[should_panic]
+fn should_not_match_all() {
+    let library = vec![
+        Book::new(1, "Database internals"),
+        Book::new(2, "Designing data intensive applications"),
+        Book::new(3, "Learning rust"),
+        Book::new(4, "Rust in action"),
+    ];
+
+    library
+        .should_not_be_empty()
+        .should_have_at_least_size(3)
+        .should_contain_all(&[
+            &Book::new(3, "Learning rust"),
+            &Book::new(4, "Designing a KV storage engine"),
+        ]);
+}
