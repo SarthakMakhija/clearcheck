8e29582c8883919b47dcf576a60cc028a51507e0
Sarthak <sarthak.makhija@gmail.com>
2024-01-10 17:33:55 +0530

    Sarthak | Adds code documentation for date

--- src/assertions/date/mod.rs
+++ src/assertions/date/mod.rs
@@ -3,26 +3,205 @@ use chrono::{Datelike, NaiveDate};
 use crate::matchers::date::{be_a_leap_year, have_same_day, have_same_month, have_same_year};
 use crate::matchers::{Should, ShouldNot};
 
+/// DateAssertion enables assertions about various properties of NaiveDate.
+///
+///
+/// It offers a fluent interface for chaining multiple assertions.
+///
+/// # Example
+/// ```
+/// use chrono::NaiveDate;
+/// use clearcheck::assertions::date::DateAssertion;
+/// use clearcheck::assertions::ordered::OrderedAssertion;
+///
+/// let date = NaiveDate::from_ymd_opt(2024, 1, 10).unwrap();
+/// date
+///     .should_be_a_leap_year()
+///     .should_have_month(1)
+///     .should_be_greater_than(&NaiveDate::from_ymd_opt(2023, 1, 10).unwrap());
+/// ```
 pub trait DateAssertion {
+
+    /// - Asserts that the date has the same year as the other date.
+    /// - Returns a reference to self for fluent chaining.
+    /// - Panics if the assertion fails.
+    /// # Example
+    /// ```
+    /// use chrono::NaiveDate;
+    /// use clearcheck::assertions::date::DateAssertion;
+    ///
+    /// let date = NaiveDate::from_ymd_opt(2024, 1, 10).unwrap();
+    /// date.should_have_same_year_as(&NaiveDate::from_ymd_opt(2024, 10, 20).unwrap());
+    /// ```
     fn should_have_same_year_as(&self, other: &NaiveDate) -> &Self;
+
+    /// - Asserts that the date does not have the same year as the other date.
+    /// - Returns a reference to self for fluent chaining.
+    /// - Panics if the assertion fails.
+    /// # Example
+    /// ```
+    /// use chrono::NaiveDate;
+    /// use clearcheck::assertions::date::DateAssertion;
+    ///
+    /// let date = NaiveDate::from_ymd_opt(2024, 1, 10).unwrap();
+    /// date.should_not_have_same_year_as(&NaiveDate::from_ymd_opt(2020, 10, 20).unwrap());
+    /// ```
     fn should_not_have_same_year_as(&self, other: &NaiveDate) -> &Self;
 
+    /// - Asserts that the date has the same year as the given year.
+    /// - Returns a reference to self for fluent chaining.
+    /// - Panics if the assertion fails.
+    /// # Example
+    /// ```
+    /// use chrono::NaiveDate;
+    /// use clearcheck::assertions::date::DateAssertion;
+    ///
+    /// let date = NaiveDate::from_ymd_opt(2024, 1, 10).unwrap();
+    /// date.should_have_year(2024);
+    /// ```
     fn should_have_year(&self, year: i32) -> &Self;
+
+    /// - Asserts that the date does not have the same year as the given year.
+    /// - Returns a reference to self for fluent chaining.
+    /// - Panics if the assertion fails.
+    /// # Example
+    /// ```
+    /// use chrono::NaiveDate;
+    /// use clearcheck::assertions::date::DateAssertion;
+    ///
+    /// let date = NaiveDate::from_ymd_opt(2024, 1, 10).unwrap();
+    /// date.should_not_have_year(2020);
+    /// ```
     fn should_not_have_year(&self, year: i32) -> &Self;
 
+    /// - Asserts that the date has the same month as the other date.
+    /// - Returns a reference to self for fluent chaining.
+    /// - Panics if the assertion fails.
+    /// # Example
+    /// ```
+    /// use chrono::NaiveDate;
+    /// use clearcheck::assertions::date::DateAssertion;
+    ///
+    /// let date = NaiveDate::from_ymd_opt(2024, 1, 30).unwrap();
+    /// date.should_have_same_month_as(&NaiveDate::from_ymd_opt(2024, 1, 20).unwrap());
+    /// ```
     fn should_have_same_month_as(&self, other: &NaiveDate) -> &Self;
+
+    /// - Asserts that the date does not have the same month as the other date.
+    /// - Returns a reference to self for fluent chaining.
+    /// - Panics if the assertion fails.
+    /// # Example
+    /// ```
+    /// use chrono::NaiveDate;
+    /// use clearcheck::assertions::date::DateAssertion;
+    ///
+    /// let date = NaiveDate::from_ymd_opt(2024, 10, 30).unwrap();
+    /// date.should_not_have_same_month_as(&NaiveDate::from_ymd_opt(2024, 1, 20).unwrap());
+    /// ```
     fn should_not_have_same_month_as(&self, other: &NaiveDate) -> &Self;
 
+    /// - Asserts that the date has the same month as the given month.
+    /// - Returns a reference to self for fluent chaining.
+    /// - Panics if the assertion fails.
+    /// # Example
+    /// ```
+    /// use chrono::NaiveDate;
+    /// use clearcheck::assertions::date::DateAssertion;
+    ///
+    /// let date = NaiveDate::from_ymd_opt(2024, 10, 30).unwrap();
+    /// date.should_have_month(10);
+    /// ```
     fn should_have_month(&self, month: u32) -> &Self;
+
+    /// - Asserts that the date does not have the same month as the given month.
+    /// - Returns a reference to self for fluent chaining.
+    /// - Panics if the assertion fails.
+    /// # Example
+    /// ```
+    /// use chrono::NaiveDate;
+    /// use clearcheck::assertions::date::DateAssertion;
+    ///
+    /// let date = NaiveDate::from_ymd_opt(2024, 10, 30).unwrap();
+    /// date.should_not_have_month(1);
+    /// ```
     fn should_not_have_month(&self, month: u32) -> &Self;
 
+    /// - Asserts that the date has the same day as the other date.
+    /// - Returns a reference to self for fluent chaining.
+    /// - Panics if the assertion fails.
+    /// # Example
+    /// ```
+    /// use chrono::NaiveDate;
+    /// use clearcheck::assertions::date::DateAssertion;
+    ///
+    /// let date = NaiveDate::from_ymd_opt(2024, 10, 30).unwrap();
+    /// date.should_have_same_day_as(&NaiveDate::from_ymd_opt(2024, 1, 30).unwrap());
+    /// ```
     fn should_have_same_day_as(&self, other: &NaiveDate) -> &Self;
+
+    /// - Asserts that the date does not have the same day as the other date.
+    /// - Returns a reference to self for fluent chaining.
+    /// - Panics if the assertion fails.
+    /// # Example
+    /// ```
+    /// use chrono::NaiveDate;
+    /// use clearcheck::assertions::date::DateAssertion;
+    ///
+    /// let date = NaiveDate::from_ymd_opt(2024, 10, 30).unwrap();
+    /// date.should_not_have_same_day_as(&NaiveDate::from_ymd_opt(2024, 1, 20).unwrap());
+    /// ```
     fn should_not_have_same_day_as(&self, other: &NaiveDate) -> &Self;
 
+    /// - Asserts that the date has the same day as the other day.
+    /// - Returns a reference to self for fluent chaining.
+    /// - Panics if the assertion fails.
+    /// # Example
+    /// ```
+    /// use chrono::NaiveDate;
+    /// use clearcheck::assertions::date::DateAssertion;
+    ///
+    /// let date = NaiveDate::from_ymd_opt(2024, 10, 30).unwrap();
+    /// date.should_have_day(30);
+    /// ```
     fn should_have_day(&self, day: u32) -> &Self;
+
+    /// - Asserts that the date does not have the same day as the other day.
+    /// - Returns a reference to self for fluent chaining.
+    /// - Panics if the assertion fails.
+    /// # Example
+    /// ```
+    /// use chrono::NaiveDate;
+    /// use clearcheck::assertions::date::DateAssertion;
+    ///
+    /// let date = NaiveDate::from_ymd_opt(2024, 10, 30).unwrap();
+    /// date.should_not_have_day(10);
+    /// ```
     fn should_not_have_day(&self, day: u32) -> &Self;
 
+    /// - Asserts that the date falls in a leap year.
+    /// - Returns a reference to self for fluent chaining.
+    /// - Panics if the assertion fails.
+    /// # Example
+    /// ```
+    /// use chrono::NaiveDate;
+    /// use clearcheck::assertions::date::DateAssertion;
+    ///
+    /// let date = NaiveDate::from_ymd_opt(2024, 10, 30).unwrap();
+    /// date.should_be_a_leap_year();
+    /// ```
     fn should_be_a_leap_year(&self) -> &Self;
+
+    /// - Asserts that the date does not fall in a leap year.
+    /// - Returns a reference to self for fluent chaining.
+    /// - Panics if the assertion fails.
+    /// # Example
+    /// ```
+    /// use chrono::NaiveDate;
+    /// use clearcheck::assertions::date::DateAssertion;
+    ///
+    /// let date = NaiveDate::from_ymd_opt(2021, 10, 30).unwrap();
+    /// date.should_not_be_a_leap_year();
+    /// ```
     fn should_not_be_a_leap_year(&self) -> &Self;
 }
 
@@ -275,7 +454,7 @@ mod tests {
 
     #[test]
     #[should_panic]
-    fn should_be_a_leap_year_but_was() {
+    fn should_not_be_a_leap_year_but_was() {
         let date = NaiveDate::from_ymd_opt(2020, 1, 10).unwrap();
         date.should_not_be_a_leap_year();
     }
