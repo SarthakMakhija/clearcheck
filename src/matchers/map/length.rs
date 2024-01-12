3fe84e8d6f4cc062199c184c85944a1e3d14e8fb
Sarthak <sarthak.makhija@gmail.com>
2024-01-12 13:30:53 +0530

    Sarthak | Adds generic type T in MapLengthMatcher

--- src/matchers/map/length.rs
+++ src/matchers/map/length.rs
@@ -1,25 +1,26 @@
 use std::collections::HashMap;
 use std::hash::Hash;
+use std::marker::PhantomData;
 
 use crate::matchers::{Matcher, MatcherResult};
 
-pub enum MapLengthMatcher {
-    Same(usize),
-    Atleast(usize),
-    Atmost(usize),
+pub enum MapLengthMatcher<T> {
+    Same(usize, PhantomData<T>),
+    Atleast(usize, PhantomData<T>),
+    Atmost(usize, PhantomData<T>),
 }
 
-impl<K: Hash + Eq, V> Matcher<HashMap<K, V>> for MapLengthMatcher {
+impl<K: Hash + Eq, V> Matcher<HashMap<K, V>> for MapLengthMatcher<K> {
     fn test(&self, collection: &HashMap<K, V>) -> MatcherResult {
         self.test_length(collection.len())
     }
 }
 
-impl MapLengthMatcher {
+impl<T> MapLengthMatcher<T> {
     fn test_length(&self, input_length: usize) -> MatcherResult {
         let message_prefix = "Map";
         match self {
-            MapLengthMatcher::Same(length) => MatcherResult::formatted(
+            MapLengthMatcher::Same(length, _) => MatcherResult::formatted(
                 input_length == *length,
                 format!(
                     "{:?} length {:?} should be {:?}",
@@ -30,7 +31,7 @@ impl MapLengthMatcher {
                     message_prefix, input_length, length
                 ),
             ),
-            MapLengthMatcher::Atleast(length) => MatcherResult::formatted(
+            MapLengthMatcher::Atleast(length, _) => MatcherResult::formatted(
                 input_length >= *length,
                 format!(
                     "{:?} length {:?} should be atleast {:?}",
@@ -41,7 +42,7 @@ impl MapLengthMatcher {
                     message_prefix, input_length, length
                 ),
             ),
-            MapLengthMatcher::Atmost(length) => MatcherResult::formatted(
+            MapLengthMatcher::Atmost(length, _) => MatcherResult::formatted(
                 input_length <= *length,
                 format!(
                     "{:?} length {:?} should be atmost {:?}",
@@ -56,16 +57,16 @@ impl MapLengthMatcher {
     }
 }
 
-pub fn have_same_length(length: usize) -> MapLengthMatcher {
-    MapLengthMatcher::Same(length)
+pub fn have_same_length<T>(length: usize) -> MapLengthMatcher<T> {
+    MapLengthMatcher::Same(length, PhantomData)
 }
 
-pub fn have_atleast_same_length(length: usize) -> MapLengthMatcher {
-    MapLengthMatcher::Atleast(length)
+pub fn have_atleast_same_length<T>(length: usize) -> MapLengthMatcher<T> {
+    MapLengthMatcher::Atleast(length, PhantomData)
 }
 
-pub fn have_atmost_same_length(length: usize) -> MapLengthMatcher {
-    MapLengthMatcher::Atmost(length)
+pub fn have_atmost_same_length<T>(length: usize) -> MapLengthMatcher<T> {
+    MapLengthMatcher::Atmost(length, PhantomData)
 }
 
 #[cfg(test)]
