877b50a1e15b226a2813d6b969edfecb21d67ff6
Sarthak <sarthak.makhija@gmail.com>
2024-01-14 11:57:12 +0530

    Sarthak | Adds documentation for range matcher

--- src/matchers/range/mod.rs
+++ src/matchers/range/mod.rs
@@ -3,6 +3,20 @@ use std::ops::{Range, RangeInclusive};
 
 use crate::matchers::{Matcher, MatcherResult};
 
+/// RangeMatcher offers a flexible way to assert whether a value falls within a specified range.
+///
+/// Supports both closed ranges (inclusive of endpoints) and half-open ranges (exclusive of the upper endpoint).
+///
+/// Works with any data type that implements the Debug and PartialOrd traits.
+///
+/// # Example
+///```
+/// use clearcheck::matchers::Matcher;
+/// use clearcheck::matchers::range::be_in_inclusive_range;
+///
+/// let matcher = be_in_inclusive_range(1..=4);
+/// assert!(matcher.test(&2).passed());
+/// ```
 pub enum RangeMatcher<T: Debug + PartialOrd> {
     Closed(&'static str, RangeInclusive<T>),
     HalfOpen(&'static str, Range<T>),
@@ -40,18 +54,22 @@ where
     }
 }
 
+/// Creates a RangeMatcher that asserts whether a value falls within the given inclusive range.
 pub fn be_in_inclusive_range<T: PartialOrd + Debug>(range: RangeInclusive<T>) -> RangeMatcher<T> {
     RangeMatcher::Closed("Value", range)
 }
 
+/// Creates a RangeMatcher that asserts whether a value falls within the given exclusive range.
 pub fn be_in_exclusive_range<T: PartialOrd + Debug>(range: Range<T>) -> RangeMatcher<T> {
     RangeMatcher::HalfOpen("Value", range)
 }
 
+/// Creates a RangeMatcher that asserts whether the length (of collection, hashmap, string) falls within the given inclusive range.
 pub fn have_length_in_inclusive_range(range: RangeInclusive<usize>) -> RangeMatcher<usize> {
     RangeMatcher::Closed("Length", range)
 }
 
+/// Creates a RangeMatcher that asserts whether the length (of collection, hashmap, string) falls within the given exclusive range.
 pub fn have_length_in_exclusive_range(range: Range<usize>) -> RangeMatcher<usize> {
     RangeMatcher::HalfOpen("Length", range)
 }
